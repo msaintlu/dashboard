b8659b33c5761e595381846d48f10f3f7ff92855
Marion Saint-Lu <25301295+msaintlu@users.noreply.github.com>
2026-04-29 17:18:35 +0200

    First layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,18 @@ import {data} from "./data";
 import { ResponsiveLineChart } from "./vizcomponents/Linechart";
 import { ResponsiveStackedAreaGraph } from "./vizcomponents/StackedAreaGraph";
 
-const MARGIN = { top: 50, right: 50, bottom: 150, left: 150 };
+const MARGIN = { top: 50, right: 50, bottom: 70, left: 70 };
 
 const colors = { 
   coal            : "#6C6460", // Dim grey
   oil             : "#837A75", // Grey
   gas             : "#A9A19E", // Silver
   nuclear         : "#611A40", // Crimson violet
-  hydro           : "#1A4C3A",  // Pine teal 
+  hydro           : "#1A4C3A", // Pine teal 
   solar           : "#2A7A5C", // Jungle teal
   wind            : "#39A77F", // Seaweed
   biofuel         : "#997143", // Toffee brown
-  other_renewable : "#76D0AF" // Turquoise
+  other_renewable : "#76D0AF"  // Turquoise
   };
 
 // Name of energy type columns
@@ -29,9 +29,13 @@ const worldData = dataPWh.filter(d => d.country === "World");
 function App() {
 
   return (
-    <div style={{ height: 800 }}>
-      {/*<ResponsiveLineChart data={worldData} columns={energyTypes} colors={colors} MARGIN={MARGIN} />*/}
-      <ResponsiveStackedAreaGraph data={worldData} columns={energyTypes} colors={colors} MARGIN={MARGIN} />
+    <div style={{ display: "flex", flexDirection: "column", height: 700}}>
+      <div style={{ width: "100%", height: "50%", backgroundColor: "none"}}>
+        <ResponsiveLineChart data={worldData} columns={energyTypes} colors={colors} MARGIN={MARGIN} />
+      </div>
+      <div style={{ width: "100%", height: "50%", backgroundColor: "none"}}>
+        <ResponsiveStackedAreaGraph data={worldData} columns={energyTypes} colors={colors} MARGIN={MARGIN} />
+      </div>
     </div>
   )
 }
